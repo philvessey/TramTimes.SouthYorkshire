da1613d5832e30f66bdc8e4bd5773ae68a41b920
Phil Vessey <philvessey@icloud.com>
2026-01-30 15:19:35 +0000

    Add initial scaffold of banner adverts

---
+++ TramTimes.Web.Site/Defaults/TelerikMediaQueryDefaults.cs
@@ -0,0 +1,7 @@
+namespace TramTimes.Web.Site.Defaults;
+
+public static class TelerikMediaQueryDefaults
+{
+    public static string Columns => "(min-width: 769px)";
+    public static string Rows => "(max-width: 768px)";
+}
---
+++ TramTimes.Web.Site/Models/TelerikLayout.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace TramTimes.Web.Site.Models;
+
+public static class TelerikLayout
+{
+    [UsedImplicitly] public static bool? Columns { get; set; }
+    [UsedImplicitly] public static bool? Rows { get; set; }
+}
