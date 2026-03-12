0d43e26d3dd55a1bc66c41e7c81a2ef6665e94ca
Phil Vessey <philvessey@icloud.com>
2026-03-12 17:50:06 +0000

    Refactor route name handling

--- TramTimes.Web.Site/Defaults/TelerikMapDefaults.cs
+++ TramTimes.Web.Site/Defaults/TelerikMapDefaults.cs
@@ -3,7 +3,7 @@ namespace TramTimes.Web.Site.Defaults;
 public static class TelerikMapDefaults
 {
     public static string Attribution => "&copy; <a href='https://openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors";
-    public static double[] Center => [53.381129, -1.470085];
+    public static double[] Center => [53.382525584, -1.468535662];
     public static double MaxZoom => 18;
     public static double MinZoom => 14;
     public static string[] Subdomains => ["a", "b", "c"];
